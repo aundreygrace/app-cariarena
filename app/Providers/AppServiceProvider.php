bfbb9948bcf8b412d328a3badde43adaf8c99d43
Aundrey Grace <aundreygrace.gmail.com>
2026-02-20 15:11:39 +0700

    update web session

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,9 +20,19 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot()
-{
-    if (app()->environment('production')) {
-        URL::forceScheme('https');
+    {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+            
+            // Fix untuk proxy Railway
+            \Illuminate\Http\Request::setTrustedProxies(
+                ['*'],
+                \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR |
+                \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST |
+                \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT |
+                \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO |
+                \Illuminate\Http\Request::HEADER_X_FORWARDED_AWS_ELB
+            );
+        }
     }
-}
 }
